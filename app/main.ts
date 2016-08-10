9b676440d6ed38d6dd31ce4ae1fd2b610d5bddc8
yo3l <yo3l.m18@gmail.com>
2016-08-10 14:21:45 -0400

    traduccion en el main

--- app/main.ts
+++ app/main.ts
@@ -1,11 +1,16 @@
 import { bootstrap }      from '@angular/platform-browser-dynamic';
-import { provide,enableProdMode }    from '@angular/core';
+import {provide, enableProdMode, PLATFORM_PIPES}    from '@angular/core';
 import { Http, HTTP_PROVIDERS } from '@angular/http';
 import { AuthConfig, AuthHttp } from 'angular2-jwt';
 import { LocationStrategy, HashLocationStrategy, APP_BASE_HREF} from '@angular/common';
 import { globalService }   from './common/globalService';
 import { AppComponent }   from './app.component';
 import { ToastsManager,ToastOptions } from 'ng2-toastr/ng2-toastr';
+import {
+    TranslateLoader, TranslateStaticLoader,
+    TranslateService
+} from 'ng2-translate/ng2-translate';
+
 import 'semantic';
 
 let options = {
@@ -18,6 +23,12 @@ bootstrap(AppComponent, [
     ToastsManager,
     provide(ToastOptions, { useValue: new ToastOptions(options)}),
     HTTP_PROVIDERS,
+    {
+        provide: TranslateLoader,
+        useFactory: (http: Http) => new TranslateStaticLoader(http, 'assets/i18n', '.json'),
+        deps: [Http]
+    },
+    TranslateService,
     provide(LocationStrategy, { useClass: HashLocationStrategy }),
     provide(APP_BASE_HREF, { useValue: '/' }),
     provide(AuthHttp, {
