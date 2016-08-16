3d7e95631394cbcee27419a3b12aa942d16f047a
daniel <oliveros.jesusd@gmail.com>
2016-08-16 14:15:56 -0400

    ts basico de ubicacion

--- app/location/location.ts
+++ app/location/location.ts
@@ -1,53 +1,179 @@
-import {Component, OnInit} from "@angular/core";
+import {Component,ViewChild, OnInit} from "@angular/core";
 import {Http} from "@angular/http";
 import {ToastsManager} from "ng2-toastr/ng2-toastr";
 import {RestController} from "../common/restController";
 import {globalService} from "../common/globalService";
 import {Tables} from "../utils/tables/tables";
+import {Save} from "../utils/save/save";
+import {TranslatePipe, TranslateService} from "ng2-translate/ng2-translate";
 
 @Component({
-    selector: 'location',
+    selector: 'location_product',
     templateUrl: 'app/location/index.html',
     styleUrls: ['app/location/style.css'],
-    directives: [Tables]
+    pipes: [TranslatePipe],
+    directives: [Tables,Save]
 })
 
 
-export class Location extends RestController implements OnInit {
+export class Location_product extends RestController implements OnInit {
 
-    public rules:any={} ;
-    public viewOptions:any={};
+    public rules: any = {};
+    public viewOptions: any = {};
     public paramsTable:any={};
+    public paramsSave :any ={};
+    public rulesSave :any={};
+    public paramsSearch :any={};
+    public rulesSearch :any={};
+    public externalSave :any={};
 
-    constructor(public http: Http, public toastr: ToastsManager, public myglobal: globalService) {
+    
+    constructor(public http: Http, public toastr: ToastsManager, public myglobal: globalService,public translate: TranslateService) {
         super(http, toastr);
-        this.setEndpoint("/consulta/warehouse.json");
-        //this.setEndpoint("/warehouse/");
+        this.setEndpoint("/ubicaciones/");
     }
 
-    ngOnInit()
-    {
-
-        let that = this;
-        this.initOptionsView();
-        this.initRules();
-        this.initParamsTable();
 
 
-        let successCallback= response => {
-            Object.assign(that.dataList,response.json())
-        }
-        this.httputils.doGet(this.endpoint,successCallback,this.error,true)
+    initLang(){
+        var userLang = navigator.language.split('-')[0]; // use navigator lang if available
+        userLang = /(es|en)/gi.test(userLang) ? userLang : 'es';
+        this.translate.setDefaultLang('en');
+        this.translate.use(userLang);
+    }
+
+
+
+    initParamsTable(){
+        this.paramsTable.endpoint=this.endpoint;
+        this.paramsTable.actions={};
+        this.paramsTable.actions.delete = {
+            "icon": "fa fa-trash",
+            "exp": "",
+            'title': 'Eliminar',
+            'permission': '1',
+            'message': 'Esta seguro de eliminar',
+            'keyAction':'description'
+        };
+        this.paramsTable.actions.print = {
+            "icon": "fa fa-print",
+            "exp": "",
+            'title': 'Imprimir',
+            'permission': '1',
+            'message': 'wii imprimir',
+            'keyAction':'description'
+        };
 
     }
+
+
+    initOptions() {
+        this.viewOptions["title"] = 'Ubicacion';
+        this.viewOptions["permissions"] = {"list": true};/*TODO PERMISO REAL this.myglobal.existsPermission('10')}*/
+        this.viewOptions["errors"] ={};
+        this.viewOptions["errors"].notFound= "no se encontraron resultados";
+        this.viewOptions["errors"].list="no tiene permisos para ver los productos";
+    }
+
 
     initRules()
     {
-        this.rules["warehoseBarCode"]={"update":false,"visible":true,"type":"text","key":"barCode","title":"Codigo de Barras del almacen","placeholder":"ingrese el codigo de barras"};
-        this.rules["name"]={"update":true,"visible":false,"type":"text","key":"name","title":"Nombre","placeholder":"ingrese nombre"};
-        this.rules["row"]={"update":true,"visible":true,"type":"text","key":"row","title":"Fila","placeholder":"ingrese la fila"};
-        this.rules["column"]={"update":true,"visible":true,"type":"text","key":"column","title":"Columna","placeholder":"ingrese la columna"};
-        this.rules["description"]={"update":true,"visible":true,"type":"textarea","key":"description","title":"Descripcion","placeholder":"ingrese la descripcion"};
+        //TODO hacer que los update se realcionen con los permisos
+        //todo las claves unicas
+        //rules de la clase
+        let update =true; /*this.myglobal.existsPermission("1");*/
+        this.rules["code"] = {
+            "update": update,
+            "visible": true,
+            'required':true,
+            'maxLength':5,
+            'icon':'fa fa-barcode',
+            "type": "text",
+            "key": "code",
+            "title": "Codigo de ubicaion",
+            "placeholder": "ingrese la ubicacion",
+            'msg':{
+                'errors':{
+                    'required':'El campo es obligatorio',
+                    'maxlength':'Maximo numero de caracteres 5'
+                },
+            },
+
+        };
+
+        this.rules["title"] = {
+            "update": update,
+            "visible": true,
+            'required':true,
+            'icon':'fa fa-barcode',
+            "type": "text",
+            "key": "title",
+            "title": "Titulo de ubicacion",
+            "placeholder": "ingrese el titulo de ubicacion",
+            'msg':{
+                'errors':{
+                    'required':'El campo es obligatorio'
+                },
+            },
+
+        };
+
+        this.rules["columna"] = {
+            "update": update,
+            "visible": true,
+            'required':true,
+            'maxLength':5,
+            'icon':'fa fa-barcode',
+            "type": "text",
+            "key": "columna",
+            "title": "Columna",
+            "placeholder": "ingrese columna",
+            'msg':{
+                'errors':{
+                    'required':'El campo es obligatorio',
+                    'maxlength':'Maximo numero de caracteres 5'
+                },
+            },
+
+        };
+
+        this.rules["fila"] = {
+            "update": update,
+            "visible": true,
+            'required':true,
+            'maxLength':5,
+            'icon':'fa fa-barcode',
+            "type": "text",
+            "key": "fila",
+            "title": "Fila",
+            "placeholder": "ingrese la fila",
+            'msg':{
+                'errors':{
+                    'required':'El campo es obligatorio',
+                    'maxlength':'Maximo numero de caracteres 5'
+                },
+            },
+
+        };
+
+
+        this.rules["id"] = {
+            "update": false,
+            "visible": false,
+            'required': true,
+            'icon': 'fa fa-barcode',
+            "type": "text",
+            "key": "fila",
+            "title": "Fila",
+            'msg': {
+                'errors': {
+                    'required': 'El campo es obligatorio'
+                },
+            },
+
+        };
+
+
     }
 
     initOptionsView()
@@ -58,13 +184,107 @@ export class Location extends RestController implements OnInit {
         this.viewOptions["errors"] = {"list": "no tiene permisos"};
     }
 
-    initParamsTable(){
-        this.paramsTable.endpoint=this.endpoint;
-        this.paramsTable.actions=[];
-        this.paramsTable.actions.delete={"icon":"fa fa-trash","exp":""};
-        this.paramsTable.actions.print={"icon":"fa fa-print","exp":""};
 
+
+    initSave() {
+        //TODO agregar los permisos
+        this.paramsSave= {
+            title: "Agregar Productos",
+            idModal: "searchProductos",
+            endpoint: this.endpoint,
+        }
+
+
+        this.rulesSave["code"] = {
+            'required':true,
+            'maxLength':5,
+            'icon':'fa fa-barcode',
+            "type": "text",
+            "key": "code",
+            "title": "Codigo ",
+            'msg':{
+                'errors':{
+                    'required':'El campo es obligatorio',
+                    'maxlength':'Maximo numero de caracteres 5'
+                },
+            },
+            "placeholder": "ingrese el codigo"
+        };
+        this.rulesSave["title"] = {
+            'required':true,
+            'icon':'fa fa-list',
+            "type": "text",
+            "key": "title",
+            "title": "Titulo",
+            'msg':{
+                'errors':{
+                    'required':'El campo es obligatorio',
+                },
+            },
+            "placeholder": "ingrese el titulo"
+        };
+
+        this.rulesSave["columna"] = {
+            'required':true,
+            'icon':'fa fa-list',
+            "type": "text",
+            "key": "columna",
+            "title": "Columna",
+            'msg':{
+                'errors':{
+                    'required':'El campo es obligatorio',
+                },
+            },
+            "placeholder": "ingrese la columna"
+        };
+
+        
+        this.rulesSave["fila"] = {
+            'required':true,
+            'icon':'fa fa-list',
+            "type": "text",
+            "key": "detail",
+            "title": "fila",
+            'msg':{
+                'errors':{
+                    'required':'El campo es obligatorio',
+                },
+            },
+            "placeholder": "ingrese la fila"
+        };
     }
+
+    ngOnInit()
+    {
+
+        this.initLang();
+        this.initOptions();
+        this.initRules();
+        this.initParamsTable();
+        this.initSave();
+    
+        this.loadData();
+    
+    }
+
+
+    @ViewChild(Tables)
+    tables:Tables;
+    asignData(data) {
+        if(this.dataList.page && this.dataList.page.length>1)
+        {
+            this.dataList.list.pop();
+        }
+        this.dataList.list.unshift(data);
+
+        if(this.tables )
+        {
+            Object.assign(this.tables.dataList,this.dataList);
+        }
+    }
+    
+    
+    
     
 
 }
