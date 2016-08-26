1c5ef4dc9858748ceb087b9bbb0a65bad017669d
daniel <oliveros.jesusd@gmail.com>
2016-08-26 18:36:15 -0400

    implementacion de clase abstracta

--- app/permissions/permissions.ts
+++ app/permissions/permissions.ts
@@ -1,11 +1,11 @@
 import {Component, OnInit,ViewChild} from "@angular/core";
 import {Http} from "@angular/http";
 import {ToastsManager} from "ng2-toastr/ng2-toastr";
-import {RestController} from "../common/restController";
 import {globalService} from "../common/globalService";
 import {Tables} from "../utils/tables/tables";
 import {TranslateService, TranslatePipe} from 'ng2-translate/ng2-translate';
 import {Save} from "../utils/save/save";
+import {BasicConfiguration} from "../common/basic-configuration";
 
 @Component({
     selector: 'permissions',
@@ -17,51 +17,27 @@ import {Save} from "../utils/save/save";
 })
 
 
-export class Permissions extends RestController implements OnInit {
+export class Permissions extends BasicConfiguration {
 
 
-    public rules:any = {};
-    public viewOptions:any = {};
     public paramsTable:any = {};
-    public paramsSave:any = {};
-    public rulesSave:any = {};
-    public paramsSearch:any ={};
-    public permissions:any ={};
 
     constructor(public http:Http, public toastr:ToastsManager, public myglobal:globalService, public translate:TranslateService) {
-        super(http, toastr);
-        this.setEndpoint("/permissions/");
+        super("PE","/permissions/",http, toastr,myglobal,translate);
     }
+
     ngOnInit() {
-        this.initLang();
-        this.initPermissions();
         this.initRules();
-
-
-        this.initSave();
         this.initOptions();
-        this.initParamsTable();
-
         this.initSearch();
         this.loadData();
 
 
-    }
-    initLang() {
-        var userLang = navigator.language.split('-')[0]; // use navigator lang if available
-        userLang = /(es|en)/gi.test(userLang) ? userLang : 'es';
-        this.translate.setDefaultLang('en');
-        this.translate.use(userLang);
+        this.initParamsTable();
     }
 
-    initPermissions(){
-        this.permissions['list']=this.myglobal.existsPermission('PE_LIST');
-        this.permissions['add']=this.myglobal.existsPermission('PE_ADD');
-        this.permissions['update']=this.myglobal.existsPermission('PE_UPDATE');
-        this.permissions['delete']=this.myglobal.existsPermission('PE_DELETE');
-        this.permissions['filter']=this.myglobal.existsPermission('PE_FILTER');
-        this.permissions['lock']=this.myglobal.existsPermission('PE_LOCK');
-    }
+
+
     initParamsTable() {
         this.paramsTable.endpoint = this.endpoint;
         this.paramsTable.actions = {};
@@ -69,22 +45,17 @@ export class Permissions extends RestController implements OnInit {
             "icon": "fa fa-trash",
             "exp": "",
             'title': 'Eliminar',
-            'permission': '1',
+            'permission': this.permissions['delete'],
             'message': 'Esta seguro de eliminar',
             'keyAction': 'title'
         };
-
     }
 
 
 
     initOptions() {
         this.viewOptions["title"] = 'Permisos';
-        this.viewOptions["errors"] ={};
-        this.viewOptions["errors"].title= "ADVERTENCIA";
-        this.viewOptions["errors"].notFound= "no se encontraron resultados";
-        this.viewOptions["errors"].list="no tiene permisos para ver los productos";
-        this.viewOptions["button"]=[];
+        
         this.viewOptions["button"].push({
             'title':'Agregar',
             'class':'btn btn-primary',
@@ -94,6 +65,10 @@ export class Permissions extends RestController implements OnInit {
     }
 
 
+    initRuleObject()
+    {
+        
+    }
 
     initRules() {
         this.rules["code"] = {
@@ -141,20 +116,7 @@ export class Permissions extends RestController implements OnInit {
                 },
             }
         };
-        this.rules["detail"] = {
-            "update": this.permissions['update'],
-            "visible": true,
-            'icon':'fa fa-list',
-            "type": "text",
-            "key": "detail",
-            "title": "Detalle",
-            "placeholder": "ingrese detalle de permiso",
-            'msg':{
-                'errors':{
-                    'required':'El campo es obligatorio',
-                },
-            }
-        };
+        
         this.rules["controlador"] = {
             "update": this.permissions['update'],
             "visible": true,
@@ -191,37 +153,14 @@ export class Permissions extends RestController implements OnInit {
 
 
 
-    initSave() {
-        //TODO agregar los permisos
-
-        this.paramsSave= {
-            title: "Agregar Permisos",
-            idModal: "savePermissions",
-            endpoint: this.endpoint,
-        }
-        this.rulesSave = this.rules;
-
-
-    }
 
 
     initSearch() {
 
-        this.paramsSearch= {
+        this.paramsSearch['title']="Permisos";
+        this.paramsSearch['idModal']="searchPermissions";
+        this.paramsSearch['placeholder']="Ingrese el permiso";
 
-            //TODO apregar el permiso
-            'permissions':'1',
-            'title': "Permisos",
-            'idModal': "searchPermissions",
-            'endpoint': "/search/permissions",
-            'placeholder': "Ingrese el permiso",
-            'label': {name: "Nombre: ", detail: "Detalle: "},
-            'msg': {
-                'errors': {
-                    'noAuthorized': 'No posee permisos para esta accion',
-                },
-            }
-        };
     }
     
     @ViewChild(Tables)
