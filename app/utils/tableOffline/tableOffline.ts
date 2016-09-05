c469c7ec9c759ef1181aae78a05a03a2fc6cb5e8
Jesus Oliveros <oliverosj@zippyttech.com>
2016-09-05 16:07:12 -0400

    basico de table offline

---
+++ app/utils/tableOffline/tableOffline.ts
@@ -0,0 +1,46 @@
+import {Component, OnInit} from "@angular/core";
+import {Http} from "@angular/http";
+import {ToastsManager} from "ng2-toastr/ng2-toastr";
+import {globalService} from "../../common/globalService";
+import {Xeditable} from "../../common/xeditable";
+import {Search} from "../search/search";
+import {Filter} from "../filter/filter";
+import {Save} from "../save/save";
+
+
+@Component({
+    selector: 'tables-offline',
+    templateUrl: 'app/utils/tableOffline/index.html',
+    styleUrls: ['app/utils/tableOffline/style.css'],
+    inputs:['rules','dataList'],
+    directives:[Xeditable,Search,Filter,Save]
+})
+
+
+export class TablesOffline implements OnInit {
+
+    public  rules:any={}
+    public  dataList =[]
+
+
+    constructor(public http:Http,public toastr: ToastsManager, public myglobal:globalService) {
+
+    }
+
+    ngOnInit()
+    {
+        
+    }
+
+
+    getkeysRules(){
+        let data=[];
+        let that=this;
+        Object.keys(this.rules).forEach((key)=>{
+                data.push(key)
+        });
+        return data;
+    }
+
+
+}
