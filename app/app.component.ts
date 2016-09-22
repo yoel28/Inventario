1d9e127fafb1f4553a64ed45223fb4f25eb2c8a9
yoel <yo3l.m18@gmail.com>
2016-09-22 16:15:48 -0400

    prod

--- app/app.component.ts
+++ app/app.component.ts
@@ -89,9 +89,7 @@ export class AppComponent extends RestController implements OnInit{
   constructor(public router: Router,http: Http,public myglobal:globalService,public toastr: ToastsManager) {
     
       super(http)
-      //let url = "http://dev.zippyttech.com:8080";
-      let url = "http://54.173.217.103:8080";
-
+      let url = "http://52.23.236.184:8080";
       localStorage.setItem('urlAPI',url+'/api');
       localStorage.setItem('url',url);
 
