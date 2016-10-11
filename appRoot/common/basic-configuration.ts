eb291bbd78988d9a497f501109e0b75d46e68b1f
yoel <yo3l.m18@gmail.com>
2016-10-11 12:03:42 -0400

    keys

--- appRoot/common/basic-configuration.ts
+++ appRoot/common/basic-configuration.ts
@@ -164,6 +164,9 @@ export abstract class  BasicConfiguration extends RestController{
         this.translate.setDefaultLang('en');
         this.translate.use(userLang);
     }
+    getObjectKeys(data){
+        return Object.keys(data);
+    }
 
 
     
