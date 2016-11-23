932e3ca0aa5cd7fb92b02d38f8e1b1832985bc74
yoel <yo3l.m18@gmail.com>
2016-11-23 12:10:45 -0400

    view base

--- appRoot/event/event.ts
+++ appRoot/event/event.ts
@@ -11,8 +11,8 @@ import { Router} from '@angular/router-deprecated';
 declare var SystemJS:any;
 @Component({
     selector: 'event',
-    templateUrl: SystemJS.map.app+'/event/index.html',
-    styleUrls: [SystemJS.map.app+'/event/style.css'],
+    templateUrl: SystemJS.map.app+'/utils/viewBase/index.html',
+    styleUrls: [SystemJS.map.app+'/utils/viewBase/style.css'],
     directives: [Tables,Save],
     pipes: [TranslatePipe],
     providers: [TranslateService]
