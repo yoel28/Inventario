90e3e39f86b009ffb9d0fc40565b143955230325
yoel <yo3l.m18@gmail.com>
2016-11-23 11:59:44 -0400

    view base

--- appRoot/company/company.ts
+++ appRoot/company/company.ts
@@ -11,8 +11,8 @@ import { Router} from '@angular/router-deprecated';
 declare var SystemJS:any;
 @Component({
     selector: 'company',
-    templateUrl: SystemJS.map.app+'/company/index.html',
-    styleUrls: [SystemJS.map.app+'/company/style.css'],
+    templateUrl: SystemJS.map.app+'/utils/viewBase/index.html',
+    styleUrls: [SystemJS.map.app+'/utils/viewBase/style.css'],
     directives: [Tables,Save],
     pipes: [TranslatePipe],
     providers: [TranslateService]
